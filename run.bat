0509e9b753208ec3d81c300a64c2365f0b91bb18
damien-piedanna <da.piedanna@gmail.com>
2021-04-27 18:06:51 +0200

    fix run.bat

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@
 start %~dp0/WebProxyServiceHost/bin/Release/WebProxyServiceHost.exe
 start %~dp0/RoutingWithBikesHost/bin/Release/RoutingWithBikesHost.exe
 
-start %~dp0/HeavyClient/bin/Debug/HeavyClient.exe
+start %~dp0/HeavyClient/bin/Release/HeavyClient.exe
 start chrome %~dp0/WebClient/index.html
